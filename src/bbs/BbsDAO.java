1b09bfe723fd0ed8cd6fe7ac78cb90a304fcfe4e
DayunKim <ekdbs628@gmail.com>
2020-05-20 08:25:37 +0900

    feat: Add delete function

--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -149,6 +149,18 @@ public class BbsDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -1;// 데이터베이스 오류
+		return -1; // 데이터베이스 오류
+	}
+	
+	public int delete(int bbsID) {
+		String SQL = "UPDATE BBS SET bbsAvailable = 0 WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
 	}
 }
